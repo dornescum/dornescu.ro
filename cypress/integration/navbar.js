00224ce9d0f0102993a2edece23232d19ee54d0d
Mihai Dornescu <dornescu@gmail.com>
2021-10-25 18:22:30 +0300

    error link sidebar href property

--- cypress/integration/navbar.js
+++ cypress/integration/navbar.js
@@ -28,13 +28,54 @@ describe('testing navbar for elements', () => {
 		// fixme not showing the right color
 		// cy.get('.ul-links > :nth-child(1) > a').should('have.css', 'background-color', '#ffd73a')
 	});
-	it.only('testing sidebar in mobile view', function () {
+	it('testing sidebar in mobile view', function () {
 		cy.viewport('iphone-xr');
 		cy.get('.toggle-nav > .fas').click();
 		// cy.get('.sidebar');
 		cy.get('#sidebar-present > .sidebar-link');
 		cy.get('#sidebar-past > .sidebar-link');
-		cy.visit("https://dornescu.ro/").contains('a', '#').should('have.attr', 'href');
+		// cy.visit("https://dornescu.ro/").contains('a', '#').should('have.attr', 'href');
 		// cy.contains("a", "#");
+	});
+	it.only('sidebar links in mobile ', function () {
+		cy.viewport('samsung-s10');
+		cy.get('.toggle-nav > .fas').click();
+		cy.get(':nth-child(1) > .sidebar-link').click();
+		cy.get('.toggle-nav > .fas').click();
+		cy.get(':nth-child(2) > .sidebar-link').click();
+		cy.get('.toggle-nav > .fas').click();
+		cy.get(':nth-child(3) > .sidebar-link').click();
+
+		// github
+		cy.get('.toggle-nav > .fas').click();
+
+		cy.get(':nth-child(4) > .sidebar-link').click();
+		// fixme not getting the link property
+		// cy.get(':nth-child(4) > .sidebar-link').should('include', 'href', 'https://github.com/dornescum.html');
+		cy.debug()
+		// cy.url().should('eq', 'https://github.com/dornescum.html');
+		// cy.get(':nth-child(4) > .sidebar-link').location('href').should('include', 'https://github.com/dornescum.html')
+		// linkedIn
+		cy.get(':nth-child(5) > .sidebar-link').click();
+	});
+	it('launch phone in mobile', function () {
+		cy.viewport('iphone-x');
+		cy.get('.toggle-nav > .fas').click();
+		//fixme how to stop after launch action?
+		// cy.get(':nth-child(6) > .sidebar-link').click();
+		cy.get(':nth-child(6) > .sidebar-link').should('have.attr', 'href');
+	});
+	it('should launch email in mobile', function () {
+		cy.viewport('iphone-x');
+		cy.get('.toggle-nav > .fas').click();
+		cy.get(':nth-child(7) > .sidebar-link').should('have.attr', 'href');
+		cy.get(':nth-child(7) > .sidebar-link').should('have.attr', 'href', 'mailto:dornescu@gmail.com');
+	});
+	it('should close sidebar', function () {
+		cy.viewport('iphone-x');
+		cy.debug();
+		cy.get('.toggle-nav > .fas' ).click();
+		cy.debug();
+		cy.get('.sidebar-close').click();
 	});
 });
